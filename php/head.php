d559c5a211faa1d764f686be26854b211ad79e28
Christian Lauf <github-ssh@christian-lauf.info>
2025-03-23 18:28:23 +0100

    Add favicon headers and images

--- php/head.php
+++ php/head.php
@@ -9,7 +9,10 @@
 <?php echo Theme::headDescription(); ?>
 
 <!-- Favicon -->
-<?php echo Theme::favicon('img/favicon.png'); ?>
+<link rel="apple-touch-icon" sizes="180x180" href="<?php echo Theme::src('apple-touch-icon.png') ?>">
+<link rel="icon" type="image/png" sizes="32x32" href="<?php echo Theme::src('favicon-32x32.png') ?>">
+<link rel="icon" type="image/png" sizes="16x16" href="<?php echo Theme::src('favicon-16x16.png') ?>">
+<link rel="manifest" href="<?php echo Theme::src('site.webmanifest') ?>">
 
 <!-- CSS: Styles for this theme -->
 <?php echo Theme::css('css/default.css'); ?>
